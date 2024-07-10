d04e494a16cb7230a91b3227c4299374200ecb85
Mouhinf <mouhopap@gmail.com>
2024-07-10 17:53:20 +0000

    Mon premier programme python

---
+++ premierProgramme.py
@@ -0,0 +1,5 @@
+print("Bienvenue chez Python Pizza Deliveries")
+size = input("Donner la taille de Pizza ")
+add_pepperoni = input("Voulez vous du pepperoni ")
+extra_cheese = input("Voulez vous du fromage 3")
+
