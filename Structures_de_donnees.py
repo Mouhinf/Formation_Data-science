ae1ac4a5167fe6db0f8445f7ce644d228ea7c89d
Mouhinf <mouhopap@gmail.com>
2024-07-22 11:36:41 +0000

    Point de contrôle des structures de données Python

---
+++ Structures_de_donnees.py
@@ -0,0 +1,11 @@
+shopping_list = ["ddd", "dnjd", "dfr"]
+t = input("Si vous aimeriez demarrer le programme tapez oui : ")
+while t == "oui" : 
+    print("Ajouter les élements de la liste")
+    print("Supprimer les élements de la liste")
+    print("Afficher les élements de la liste")
+    t = input("Si vous aimeriez demarrer le programme tapez oui : ")
+
+for i in shopping_list :
+    i = input("Faites une selection dans le menu ")
+print(shopping_list)
