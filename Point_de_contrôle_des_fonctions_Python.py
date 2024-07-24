63729b69ae4b09718c53f591d50147a79efbadfb
Mouhinf <mouhopap@gmail.com>
2024-07-24 22:36:36 +0000

    Point de contrôle des fonctions python

--- Point_de_contrôle_des_fonctions_Python.py
+++ Point_de_contrôle_des_fonctions_Python.py
@@ -16,16 +16,23 @@ def division(a, b) :
 operation = {ajouter : "+", soustraire : "-", multiplier : "x", division : "/"} 
  
 def calculatrice(a) :
-    int(input("Saisissez le premier nombre : "))
-
+    b = int(input("Saisissez le premier nombre : "))
+    return b
 for op in operation.values() :
     print(op)
 
 while op == "+" or op == "-" or op == "/" or op == "x" :
     op = input("Donner l'opérateur que vous choisissez pour le programme :")
-    n = int(input("Donner le deuxieme numéro "))
-    calculation_function = calculatrice(n)
-    
-
-
-
+    m = int(input("Donner le deuxieme numéro "))
+    print(m)
+    calculation_function = calculatrice(8)
+    if op == "+" : 
+        reponse = ajouter(calculation_function, m )
+    if op == "-" :
+        reponse = soustraire(calculation_function, m ) 
+    if op == "x" : 
+        reponse = multiplier(calculation_function, m )
+    if op == "/" : 
+        reponse = division(calculation_function, m )
+    print(calculation_function, op, m,"=", reponse)
+    rep = input("Aimeriez vous ituliser  le resultat comme premier nombre pour d'autres calculs ? Tapez 'oui' ou 'non'"
