c280bc6fe1f7a718df09dce1d09337fed3a9fb79
Mouhinf <mouhopap@gmail.com>
2024-07-24 23:07:01 +0000

    Point de controle des fonctions Python

--- Point_de_contrôle_des_fonctions_Python.py
+++ Point_de_contrôle_des_fonctions_Python.py
@@ -15,17 +15,17 @@ def division(a, b) :
 
 operation = {ajouter : "+", soustraire : "-", multiplier : "x", division : "/"} 
  
-def calculatrice(a) :
+def calculatrice() :
     b = int(input("Saisissez le premier nombre : "))
     return b
 for op in operation.values() :
     print(op)
-
-while op == "+" or op == "-" or op == "/" or op == "x" :
+should_continue = True
+while should_continue:
     op = input("Donner l'opérateur que vous choisissez pour le programme :")
     m = int(input("Donner le deuxieme numéro "))
     print(m)
-    calculation_function = calculatrice(8)
+    calculation_function = calculatrice()
     if op == "+" : 
         reponse = ajouter(calculation_function, m )
     if op == "-" :
@@ -35,4 +35,17 @@ while op == "+" or op == "-" or op == "/" or op == "x" :
     if op == "/" : 
         reponse = division(calculation_function, m )
     print(calculation_function, op, m,"=", reponse)
-    rep = input("Aimeriez vous ituliser  le resultat comme premier nombre pour d'autres calculs ? Tapez 'oui' ou 'non'"
+    rep = input("Aimeriez vous ituliser  le resultat comme premier nombre pour d'autres calculs ? Tapez 'oui' ou 'non'")
+    if rep == "oui" or rep == "Oui" or rep == "OUI": 
+        if op == "+" : 
+            reponse = ajouter(calculation_function, reponse )
+        if op == "-" :
+            reponse = soustraire(calculation_function, reponse ) 
+        if op == "x" : 
+            reponse = multiplier(calculation_function, reponse)
+        if op == "/" : 
+            reponse = division(calculation_function, reponse)
+    else :
+        should_continue = False
+        calculatrice()
+       
