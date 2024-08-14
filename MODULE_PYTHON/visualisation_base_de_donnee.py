77d4ebed0fb6636ae35192735a004c309dc28460
Mouhinf <mouhopap@gmail.com>
2024-08-14 22:54:23 +0000

    Visualisation avec une base de donnée

---
+++ MODULE_PYTHON/visualisation_base_de_donnee.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("Popular_Baby_Names.csv")
+print(df.head())
+plt.bar(df['Year of Birth'], df['Count'], edgecolor = "black")
+plt.title("Diagramme en barre des années aniversaire en fonction des count")
+plt.xlabel("Annéé d'anniversaire")
+plt.ylabel("Nombre de bébé")
+plt.show()
