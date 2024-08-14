bf8cf593ecb6abf73cf4ba45584a7938664bee8e
Mouhinf <mouhopap@gmail.com>
2024-08-14 16:18:58 +0000

    Graphique en ligne

--- MODULE_PYTHON/graphique_en_line.py
+++ MODULE_PYTHON/graphique_en_line.py
@@ -6,4 +6,6 @@ plt.title("Graphique en line")
 plt.plot(x,y)
 plt.xlabel("x")
 plt.ylabel("y")
+plt.annotate('Peak', xy=(4, 30), xytext=(3, 28),
+             arrowprops=dict(facecolor='black', shrink=0.05))
 plt.show()
